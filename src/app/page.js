f705181a54c7741ac47ced1be8f7272c797c332a
Abhinav Anand <abhinav011501@gmail.com>
2024-11-11 10:13:14 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import TestimonialSection from "@/Components/Testimonials";
 
 export default function Home() {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-green-400">
             <main className="">
                 <BannerPage />
                 <AboutUs />
